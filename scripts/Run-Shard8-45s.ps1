7ccfac2309e02926c83efadd44d2db2f283c3d8b
Marc-Andre Nocturne <asleepcoding@outlook.com>
2026-05-21 11:10:54 +0000

    Tidy scripts: remove one-time helper scripts and clean empty tracking folders

--- scripts/Run-Shard8-45s.ps1
+++
@@ -1,3 +0,0 @@
-#Requires -Version 7
-Import-Module 'C:\dev\winget-tools\WingetTools\WingetTools.psd1' -Force
-Get-Content 'C:\dev\winget-tools\shards\shard-0008.txt' | Install-TrackedWingetPackage -TrackingDir 'C:\dev\winget-tools\tracking\shard-0008' -TimeoutSeconds 45
